f868e88d0e37028a53c8c37186a4afb2c57255c9
forhadhossen1 <mr.forhad1030@gamil.com>
2024-02-14 22:01:53 +0600

    impliment reducer

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux";
 
 const Counter = () => {
+    const { count } = useSelector((state) => state.counter.count)
     return (
         <div>
-           <p>Counter </p> 
+            <button>Increment</button>
+            <div>
+                <h1>{count}</h1>
+            </div>
+            <button>Decrement</button>
         </div>
     );
 };
