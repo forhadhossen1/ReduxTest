85b8de983d2c4d60ef009105a551ba8b2be6472f
forhadhossen1 <mr.forhad1030@gamil.com>
2024-02-14 22:34:26 +0600

    impliment  action

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,16 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {increment, decrement} from '../redux/features/counter/counterSlice';
 
 const Counter = () => {
-    const { count } = useSelector((state) => state.counter.count)
+    const { count } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
     return (
         <div>
-            <button>Increment</button>
+            <button onClick={()=> dispatch(increment())}>Increment</button>
             <div>
                 <h1>{count}</h1>
             </div>
-            <button>Decrement</button>
+            <button onClick={()=> dispatch(decrement())}>Decrement</button>
         </div>
     );
 };
